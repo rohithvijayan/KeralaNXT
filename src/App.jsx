60fb4418176bf57058329bd171fb2ce9c0be2336
Rohith Vijayan <rohithvij20@gmail.com>
2026-01-21 22:52:19 +0530

    Added Bottm navigation for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import DistrictPage from './pages/DistrictPage'
 import AboutPage from './pages/AboutPage'
 import ProjectsPage from './pages/ProjectsPage'
 import InitiativesPage from './pages/InitiativesPage'
+import BottomNav from './components/BottomNav'
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/district/:districtId" element={<DistrictPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/initiatives" element={<InitiativesPage />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/district/:districtId" element={<DistrictPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/initiatives" element={<InitiativesPage />} />
+            </Routes>
+            <BottomNav />
+        </>
     )
 }
 
 export default App
+
 
 
---
+++ src/components/BottomNav.jsx
@@ -0,0 +1,32 @@
+import { NavLink, useLocation } from 'react-router-dom'
+import './BottomNav.css'
+
+const navItems = [
+    { path: '/', icon: 'home', label: 'Home' },
+    { path: '/projects', icon: 'construction', label: 'Projects' },
+    { path: '/initiatives', icon: 'verified', label: 'Policies' },
+    { path: '/about', icon: 'info', label: 'About' }
+]
+
+function BottomNav() {
+    const location = useLocation()
+
+    return (
+        <nav className="bottom-nav">
+            {navItems.map((item) => (
+                <NavLink
+                    key={item.path}
+                    to={item.path}
+                    className={({ isActive }) =>
+                        `bottom-nav-item ${isActive ? 'active' : ''}`
+                    }
+                >
+                    <span className="material-symbols-outlined">{item.icon}</span>
+                    <span className="bottom-nav-label">{item.label}</span>
+                </NavLink>
+            ))}
+        </nav>
+    )
+}
+
+export default BottomNav
