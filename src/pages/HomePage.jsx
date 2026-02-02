c1fe19d5f8500b8b02c9dfbd44eb4a2aa4ebbdbb
Rohith Vijayan <rohithvij20@gmail.com>
2026-02-02 20:10:39 +0530

    updated popup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -344,14 +344,14 @@ function HomePage() {
                                     <span className="material-symbols-outlined">search</span>
                                 </div>
                                 <h2>Kerala Budget — real Kerala story 🔍</h2>
-                                <p>evide paisa kitti , evide paisa poyi,— ellam njan paranj theram .</p>
+                                <p>Where We Spend, Where We Gained-Everything At Once Place.</p>
                                 <div className="popup-btn-group">
-                                    <Link to="/budget-highlights" className="popup-cta-btn highlight">
+                                    <Link to="/union-budget-glance" className="popup-cta-btn highlight">
                                         <span className="material-symbols-outlined">auto_awesome</span>
-                                        Top Highlights
+                                        Union Budget 
                                     </Link>
                                     <Link to="/state-budget?year=2026-27" className="popup-cta-btn secondary">
-                                        Check Budget Details
+                                        Kerala Budget
                                         <span className="material-symbols-outlined">arrow_forward</span>
                                     </Link>
                                 </div>
